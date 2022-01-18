3f0d05e1d93213f2496dcdebb6f29d93d10d2938
BrookeBryski <brookeabryski@gmail.com>
2022-01-18 17:03:42 -0600

    astrology class

--- src/main/java/com/example/SpringBootAstrologyApi/Controller/Astrology.java
+++ src/main/java/com/example/SpringBootAstrologyApi/Controller/Astrology.java
@@ -1,4 +1,26 @@
 package com.example.SpringBootAstrologyApi.Controller;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Astrology {
+
+    private long id;
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
